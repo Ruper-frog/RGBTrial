ae42c4fccab2214ebc08ddd497a53dbf7b6608f1
Ruper Ottolenghi <ruper120406@gmail.com>
2023-05-20 01:29:25 +0300

    we made a bunch of funtions that work great

--- RGBTrial/Program.cs
+++ RGBTrial/Program.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 
 namespace RGBTrial
 {
     internal class Program
     {
+        static int CellSize = 32, ImageSize = 100;
         static void Main(string[] args)
         {
+            SortImages("Trash");
+
+            return;
+            /*
+
             // Load the small images
             Bitmap[] images = new Bitmap[100];
 
             for (int i = 0; i < 100; i++)
-                images[i] = new Bitmap("C:\\Users\\USER\\OneDrive\\שולחן העבודה\\Ruper\\wallpapersden.com_kali-linux-matrix_1980x1080.jpg");
+                images[i] = new Bitmap("C:\\Users\\ruper\\OneDrive\\שולחן העבודה\\Ruper\\somthing somthing 1997.jpg");
 
 
             // Set the target width and height
-            int targetWidth = 1920 * 10;
-            int targetHeight = 1080 * 10;
+            int targetWidth = images[0].Width * 10 * 3;
+            int targetHeight = images[0].Height * 10 * 3;
 
             // Set the locations of the small images on the new image
             Point[] locations = new Point[100];
@@ -29,7 +36,7 @@ namespace RGBTrial
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    locations[Index++] = new Point(1920 * i, 1080 * j);
+                    locations[Index++] = new Point(images[0].Width * i, images[0].Height * j);
                 }
             }
 
@@ -37,7 +44,8 @@ namespace RGBTrial
             Bitmap combinedImage = CombineImages(images, targetWidth, targetHeight, locations);
 
             // Save the result to a file
-            combinedImage.Save("C:\\Users\\USER\\OneDrive\\שולחן העבודה\\New folder (2)\\Combined Image.jpg", ImageFormat.Jpeg);
+            combinedImage.Save("C:\\Users\\ruper\\OneDrive\\שולחן העבודה\\Ruper\\Combined Image.jpg", ImageFormat.Jpeg);
+            */
 
             return;
             /*
@@ -61,6 +69,7 @@ namespace RGBTrial
 
             FindPixle(Location);*/
         }
+        /*
         static void FindPixle(string URL)
         {
             // Load image file
@@ -134,7 +143,7 @@ namespace RGBTrial
         public static Bitmap CombineImages(Bitmap[] images, int targetWidth, int targetHeight, Point[] locations)
         {
             // Create a new Bitmap object to hold the combined image
-            Bitmap result = new Bitmap(targetWidth, targetHeight);
+            Bitmap result = new Bitmap(20000, 20000);
 
             using (Graphics g = Graphics.FromImage(result))
             {
@@ -162,6 +171,127 @@ namespace RGBTrial
                     // Draw the image onto the new image
                     g.DrawImage(images[i], new Rectangle(x, y, width, height));
                 }
+            }
+            return result;
+        }*/
+        static void MinImage(string ImageURL)
+        {
+            Image Photo = Image.FromFile(ImageURL);
+
+            float ratio = Math.Max((float)ImageSize / Photo.Width, (float)ImageSize / Photo.Height);
+            int width = (int)(Photo.Width * ratio);
+            int height = (int)(Photo.Height * ratio);
+
+            Bitmap image = new Bitmap(Photo, width, height);
+
+            Bitmap result = new Bitmap(ImageSize, ImageSize);
+
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.Clear(Color.White);
+
+                g.DrawImage(image, new Rectangle(-(width - ImageSize) / 2, -(height - ImageSize) / 2, width, height));
+            }
+
+            Photo.Dispose();
+
+            result.Save(ImageURL.Replace(GetImageName(ImageURL), "NewImage.jpg"), ImageFormat.Png);
+
+            image.Dispose();
+            File.Delete(ImageURL);
+
+            result.Dispose();
+        }
+        static void SortImages(string ImagesLocation)
+        {
+            List<string> files = new List<string>(Directory.GetFiles(ImagesLocation));
+
+            foreach (string File in files)
+            {
+                MinImage(File);
+                
+                //ImageToCell(File);
+            }
+        }
+        static void ImageToCell(string Image)
+        {
+            Color Cell = ImageCell(Image);
+
+            string CellName = ColorToString(Cell);
+
+            if (!Directory.Exists(CellName))
+            {
+                // Create the new directory
+                Directory.CreateDirectory(CellName);
+            }
+
+            string ImageName = GetImageName(Image);
+
+            string Destebation = Path.Combine(CellName, ImageName);
+
+            File.Move(Image, Destebation);
+        }
+        static string ColorToString(Color Color)
+        {
+            return $"{Color.R}_{Color.G}_{Color.B}";
+        }
+        static string GetImageName(string Image)
+        {
+            int BackSlashIdex = Image.LastIndexOf("\\");
+
+            return Image.Substring(BackSlashIdex + 1, Image.Length - BackSlashIdex - 1);
+        }
+        static Color ImageCell(string ImageURL)
+        {
+            Bitmap image = new Bitmap(ImageURL);
+
+            int R = 0, G = 0, B = 0;
+
+            for (int x = 0; x < image.Height; x++)
+            {
+                for (int y = 0; y < image.Width; y++)
+                {
+                    Color Pixel = image.GetPixel(y, x);
+
+                    R += Pixel.R;
+                    G += Pixel.G;
+                    B += Pixel.B;
+                }
+            }
+            R /= (image.Height * image.Width);
+            G /= (image.Height * image.Width);
+            B /= (image.Height * image.Width);
+
+            image.Dispose();
+
+            return CellRGB(R, G, B);
+        }
+        static Color CellRGB(int R, int G, int B)
+        {
+            R /= CellSize;
+            G /= CellSize;
+            B /= CellSize;
+
+            R *= CellSize;
+            G *= CellSize;
+            B *= CellSize;
+
+            R += CellSize / 2;
+            G += CellSize / 2;
+            B += CellSize / 2;
+
+            Color RGB = Color.FromArgb(R, G, B);
+
+            return RGB;
+        }
+        static Bitmap NewCanvas(int Height, int Width)
+        {
+            Bitmap result = new Bitmap(Height, Width);
+
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                // Clear the new image with a white background
+                g.Clear(Color.White);
             }
             return result;
         }
